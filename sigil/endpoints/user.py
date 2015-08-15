890be7c93c48cbff46a440dc18e9c3b0f708c3b0
Eric Gazoni <eric.gazoni@adimian.com>
2015-08-16 00:01:18 +0200

    add user get details

---
+++ sigil/endpoints/user.py
@@ -0,0 +1,19 @@
+from . import ManagedResource, reqparse
+from ..utils import current_user
+from ..models import User
+
+
+class UserDetails(ManagedResource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('username', type=str)
+        args = parser.parse_args()
+
+        if args['username']:
+            user = User.by_username(args['username'])
+        else:
+            user = current_user
+
+        return {'firstname': user.fn,
+                'lastname': user.sn,
+                'username': user.username}

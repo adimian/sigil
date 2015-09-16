e5085c58b5b81375b159098c03363399d531d0d2
Eric Gazoni <eric.gazoni@adimian.com>
2015-09-16 11:00:28 +0200

    smaller version info

--- ui/js/app/sigil.utils.js
+++ ui/js/app/sigil.utils.js
@@ -36,7 +36,7 @@ var ServerOptions = function() {
     var self = this;
     self.use_totp = ko.observable();
     self.auth_token_name = ko.observable();
-    self.version = ko.observable();
+    self.version = ko.observable('loading ...');
     $.getJSON(SIGIL_API + '/options', function(data) {
         self.use_totp(data.use_totp == "1");
         self.auth_token_name(data.auth_token);

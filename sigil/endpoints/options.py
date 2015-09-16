bf19f73fa0e0d7a4f78476817dcb5ad28a875fc0
Eric Gazoni <eric.gazoni@adimian.com>
2015-09-16 10:58:11 +0200

    add version number in ui

--- sigil/endpoints/options.py
+++ sigil/endpoints/options.py
@@ -1,8 +1,11 @@
-from ..endpoints import AnonymousResource
 from flask import current_app as app
+
+from .. import __version__
+from ..endpoints import AnonymousResource
 
 
 class ServerOptions(AnonymousResource):
     def get(self):
         return {'use_totp': app.config['ENABLE_2FA'],
-                'auth_token': app.config['SESSION_TOKEN_HEADER']}
+                'auth_token': app.config['SESSION_TOKEN_HEADER'],
+                'version': __version__}
--- ui/js/app/sigil.utils.js
+++ ui/js/app/sigil.utils.js
@@ -36,9 +36,11 @@ var ServerOptions = function() {
     var self = this;
     self.use_totp = ko.observable();
     self.auth_token_name = ko.observable();
+    self.version = ko.observable();
     $.getJSON(SIGIL_API + '/options', function(data) {
         self.use_totp(data.use_totp == "1");
         self.auth_token_name(data.auth_token);
+        self.version(data.version);
     });
 };
 

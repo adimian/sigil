70051f2a7943b1f09ad3b719e3fbbc48aa1654f0
Maarten De Paepe <maarten@adimian.com>
2015-09-24 08:24:27 +0200

    linked the api recovery to the ui

---
+++ ui/js/app/recover.js
@@ -0,0 +1,42 @@
+"use strict"
+var UserAccount = function() {
+    var self = this;
+    self.email = ko.observable();
+};
+
+var RecoverAccountApplication = function() {
+    var self = this;
+    self.server_options = new ServerOptions();
+    self.user_account = new UserAccount();
+
+    self.redirect = function() {
+        window.location = location.origin;
+    }
+
+    self.recover = function() {
+        var data = {
+            email: self.user_account.email()
+        };
+        
+        var success = function(data) {
+            $('#recover-modal').modal('show');
+        };
+
+        $.ajax({
+            method: "GET",
+            url: SIGIL_API + '/user/recover',
+            data: data,
+            success: success
+        }).error(function(data){
+            console.log(data);
+        });
+    };
+};
+
+var init = function() {
+    var app = new RecoverAccountApplication();
+    ko.applyBindings(app);
+    window.app = app;
+};
+
+$(init);

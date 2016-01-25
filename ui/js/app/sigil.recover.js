e3b28d5263896c9a379827b969269dff27ccd498
Eric Gazoni <eric.gazoni@adimian.com>
2016-01-25 14:38:15 +0100

    better recovery ui

--- ui/js/app/sigil.recover.js
+++ ui/js/app/sigil.recover.js
@@ -9,27 +9,42 @@ var RecoverAccountApplication = function() {
     self.server_options = new ServerOptions();
     self.user_account = new UserAccount();
 
+    self.error_message = ko.observable(null);
+
     self.redirect = function()  {
         window.location = app_root_redirect();
     }
 
     self.recover = function() {
+        self.error_message(null);
+
         var data = {
             email: self.user_account.email()
         };
 
-        var success = function(data) {
-            $('#recover-modal').modal('show');
-        };
+        if (self.user_account.email() === undefined || !self.user_account.email().length)  {
 
-        $.ajax({
-            method: "GET",
-            url: SIGIL_API + '/user/recover',
-            data: data,
-            success: success
-        }).error(function(data) {
-            console.log(data);
-        });
+            self.error_message("Please provide identification");
+
+        } else {
+
+            var success = function(data) {
+                $('#recover-modal').modal('show');
+            };
+
+            $.ajax({
+                method: "GET",
+                url: SIGIL_API + '/user/recover',
+                data: data,
+                success: success
+            }).error(function(data) {
+                if (data.status == 404)  {
+                    self.error_message('E-mail or username not found');
+                }
+                console.log(data);
+            });
+
+        }
     };
 };
 

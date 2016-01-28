b97515ba48e8968e061fc5d41ca5e1682be59621
Eric Gazoni <eric.gazoni@adimian.com>
2016-01-28 20:55:39 +0100

    add test on group member removal in LDAP

--- tests/test_ldap.py
+++ tests/test_ldap.py
@@ -1,7 +1,7 @@
 import json
 from unittest.mock import call
 
-from sigil.ldap import setup_auto_sync
+from sigil.ldap import setup_auto_sync, update_ldap
 
 
 def test_update_ldap_user(client, mocker):
@@ -55,3 +55,53 @@ def test_update_ldap_group(client, mocker):
                                   {'uniqueMember': [('MODIFY_ADD',
                                                      ['cn=bernard,ou=Users,dc=mycorp,dc=com'])]})),
                             any_order=True)
+
+
+def test_update_ldap_group_remove_users(client, mocker):
+    ''' need to manually call update_ldap() because of how sqlalchemy is tied
+    to the session, the automatic ldap updates only occurs once per session
+    which occurs properly when dealing with tornado-based requests, but not
+    the flask test client
+    '''
+    with client._app.app_context():
+
+        # create group
+
+        rv = client.post('/group',
+                         data={'name': 'jabber'},
+                         headers=client._auth_headers)
+        assert rv.status_code == 200, str(rv.data)
+
+        # add members
+        rv = client.post('/group/members',
+                         data={'name': 'jabber',
+                               'usernames': json.dumps(['alice', 'bernard'])},
+                         headers=client._auth_headers)
+        assert rv.status_code == 200, str(rv.data)
+
+        mocker.patch('sigil.ldap.LDAPConnection._connect')
+        mocker.patch('sigil.ldap.LDAPConnection.check')
+        update_ldap()
+
+        # remove one member
+
+        rv = client.delete('/group/members',
+                           data={'name': 'jabber',
+                                 'usernames': json.dumps(['alice'])},
+                           headers=client._auth_headers)
+        assert rv.status_code == 200, str(rv.data)
+
+        connect = mocker.patch('sigil.ldap.LDAPConnection._connect')
+        mocker.patch('sigil.ldap.LDAPConnection.check')
+        update_ldap()
+
+        modify = connect().modify
+
+        assert modify.mock_calls.count(call('cn=jabber,ou=Groups,dc=mycorp,dc=com',
+                                            {'uniqueMember': [('MODIFY_ADD',
+                                                               ['cn=bernard,ou=Users,dc=mycorp,dc=com'])]})) == 1
+
+        assert modify.mock_calls.count(call('cn=jabber,ou=Groups,dc=mycorp,dc=com',
+                                            {'uniqueMember': [('MODIFY_ADD',
+                                                               ['cn=alice,ou=Users,dc=mycorp,dc=com'])]})) == 0
+
